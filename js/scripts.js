9b62a2a8739ad1d83fed955a0a1ca163154928b2
Gabriel Rios <gabrielrios@Gabriels-MacBook-Air.local>
2019-02-28 19:23:50 -0800

    added sidebar

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function sidebarToggleOpen(){
+    document.getElementById('sidebar').style.width="200px";
+    }
+    function sidebarToggleClose(){
+    document.getElementById('sidebar').style.width="0";
+    document.getElementById('closeSidebar').style.cursor="pointer";
+    }
+    
+    
