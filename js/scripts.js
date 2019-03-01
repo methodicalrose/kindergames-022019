edfa0845e8c8512947d8337a8924c19a208dce0e
Gabriela T <gtrujillo15@gmail.com>
2019-02-28 19:46:37 -0800

    testing

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-function sidebarToggleOpen(){
-    document.getElementById('sidebar').style.width="200px";
-    }
-    function sidebarToggleClose(){
-    document.getElementById('sidebar').style.width="0";
-    document.getElementById('closeSidebar').style.cursor="pointer";
-    }
-    
-    
