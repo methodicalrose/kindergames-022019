7ac08d966bbcb92ffb11e44d5a51321e402c7731
Gabriela T <gtrujillo15@gmail.com>
2019-03-05 20:58:41 -0800

    researched different methods for js code

--- js/reading.js
+++ js/reading.js
@@ -1,7 +1,5 @@
-//NOTES FOR JS
-/* my thinking is that when one containers is clicked, whether it's the pic or the text, the container will make the border color change and will "blink" while it is waiting for one of the pic/text ones to be clicked. Then when it is picked, the new blinking color will become permanent. Three different picked for the three choices. once every choice has been made, it will then let them know if they got them correct or if they need to try again.
-initially, i wanted a line to connect the pic/text to the matching pic/text but i googled that it would require svg which might be easy but i want to stick to JS*/
+const flash = document.getElementById('here');
 
-// DEFINE VARIABLES
-const btn = document.querySelector('.game-card');
-
+here.addEventListener("click", function(){
+    document.body.style.backgroundColor = "red";
+  });
