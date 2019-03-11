89e594fca9fc064daffccddcdb2ac8359a6a022c
Gabriela T <gtrujillo15@gmail.com>
2019-03-11 00:00:23 -0700

    figured out how to addeventlistener to all divs and am looking into toggle for clicking the div more than once. may just try through js instead of adding class toggle

--- js/reading.js
+++ js/reading.js
@@ -1,3 +1,28 @@
-function changeColor() {
-  document.querySelectorAll("readpic-card").style.borderColor = "red";
+const carp = document.getElementById("carpic");
+const cart = document.getElementById("cartext");
+const catp = document.getElementById("catpic");
+const catt = document.getElementById("cattext");
+const doorp = document.getElementById("doorpic");
+const doort = document.getElementById("doortext");
+
+carp.addEventListener("click", red);
+cart.addEventListener("click", red);
+
+function red (){
+  // this.style.borderColor = "red";
+  this.classList.toggle("borderBlink");
+}
+
+catp.addEventListener("click", purple);
+catt.addEventListener("click", purple);
+
+function purple (){
+  this.style.borderColor = "purple";
+}
+
+doorp.addEventListener("click", yellow);
+doort.addEventListener("click", yellow);
+
+function yellow (){
+  this.style.borderColor = "yellow";
 }
